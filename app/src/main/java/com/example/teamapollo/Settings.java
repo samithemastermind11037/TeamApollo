534bddeaf7a911cee1453f5fc579abf2d53e0d4e
samithemastermind11037 <81188736+samithemastermind11037@users.noreply.github.com>
2021-03-28 13:03:40 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/teamapollo/Settings.java
@@ -0,0 +1,9 @@
+package com.example.teamapollo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.os.Bundle;
+
+public class Settings extends AppCompatActivity{
+
+}
