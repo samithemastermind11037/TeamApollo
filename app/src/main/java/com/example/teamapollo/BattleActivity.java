4b81b20aeb29ae88eab30c4b74c81072b3ea3f72
samithemastermind11037 <81188736+samithemastermind11037@users.noreply.github.com>
2021-03-31 14:52:32 -0400

    Create BattleActivity.java

---
+++ app/src/main/java/com/example/teamapollo/BattleActivity.java
@@ -0,0 +1,10 @@
+package com.example.teamapollo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class BattleActivity extends AppCompatActivity {
+
+}
