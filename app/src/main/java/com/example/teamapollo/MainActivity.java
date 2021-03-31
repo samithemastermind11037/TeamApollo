67b0ea4b917759709fec8132a0497baa19d3efa7
samithemastermind11037 <81188736+samithemastermind11037@users.noreply.github.com>
2021-03-31 14:54:14 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/teamapollo/MainActivity.java
+++ app/src/main/java/com/example/teamapollo/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.teamapollo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button start;
+    Button settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.buttonStartGame);
+        settings = (Button) findViewById(R.id.buttonSettings);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(getApplicationContext(), BOARD_MODE.class);
+                //startActivity(intent);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Settings.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
