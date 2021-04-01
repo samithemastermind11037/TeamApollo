50fd93e869e11aada3e9608960b0924dcadb3373
samithemastermind11037 <81188736+samithemastermind11037@users.noreply.github.com>
2021-04-01 15:06:23 -0400

    Update Settings.java

--- app/src/main/java/com/example/teamapollo/Settings.java
+++ app/src/main/java/com/example/teamapollo/Settings.java
@@ -3,7 +3,36 @@ package com.example.teamapollo;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class Settings extends AppCompatActivity{
+    Button returnStart;
+    Button orientation;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+
+        returnStart = (Button) findViewById(R.id.returnButton);
+        orientation = (Button) findViewById(R.id.orientationButton);
+
+        returnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        orientation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(getApplicationContext(), Settings.class);
+                //startActivity(intent);
+            }
+        });
+
+    }
 }
